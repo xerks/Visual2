4b55b818bd56d2002d5a78234f247533d6c34fbb
xerks19 <xerks@gmx.at>
2020-06-25 19:47:18 +0200

    modified light and obj

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -4,13 +4,17 @@ public class Rotate : MonoBehaviour
 {
     public float rotSpeed = 30f;
     public bool rotatX = true;
+    public bool rotatY = true;
 
     void OnMouseDrag()
     {
         float rotX = -Input.GetAxis("Mouse X") * rotSpeed;
         float rotY = -Input.GetAxis("Mouse Y") * rotSpeed;
 
-        transform.rotation *= Quaternion.AngleAxis(rotX, Vector3.up);
+        if (rotatY)
+        {
+            transform.rotation *= Quaternion.AngleAxis(rotX, Vector3.up);
+        }
         if (rotatX)
         {
             transform.rotation *= Quaternion.AngleAxis(rotY, Vector3.right);
